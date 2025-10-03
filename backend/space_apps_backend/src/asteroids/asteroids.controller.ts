22a29e20ea8b596da5a92b7dda97fa5ad7390d98
Jorge Fernández Marín <jofmar00@gmail.com>
2025-10-04 00:17:05 +0200

    Back epico

--- backend/space_apps_backend/src/asteroids/asteroids.controller.ts
+++ backend/space_apps_backend/src/asteroids/asteroids.controller.ts
@@ -6,20 +6,27 @@ import type { Response } from 'express';
 export class AsteroidsController {
     constructor(private readonly asteroidService: AsteroidsService){}
 
-    @Get('diameter/:id')
-    getDiameterById(@Param('id') id: number) {
-        return this.asteroidService.getDiameterById(id);
+    @Get('info/:id')
+    getAsteroidInfo(@Param('id') id: number) {
+        return this.asteroidService.getAsteroidInfo(id);
     }
 
     @Get('image')
     async getImage(
         @Query('longitude') longitude: number,
         @Query('latitude') latitude: number,
+        @Query('diameter') diameter: number,
         @Res() res: Response,
     ) {
-        console.log(`Longitude: ${longitude}, latitude: ${latitude}`)
-        const image = await this.asteroidService.getMapImage(latitude, longitude, 1);
+        console.log(`Longitude: ${longitude}, latitude: ${latitude}, diametro ${diameter}`)
+        const image = await this.asteroidService.getMapImage(latitude, longitude, diameter);
         res.setHeader('Content-Type', 'image/png'); // <-- clave para que el navegador lo renderice
         res.send(image);
+    }
+
+    @Get('explosion')
+    getExplosionRadius(@Query('velocity') velocity: number, @Query('mass') mass: number) {
+        console.log(`Llamando con velocity: ${velocity}, mass: ${mass}`);
+        return this.asteroidService.getExplosionRadius(velocity, mass);
     }
 }
--- backend/space_apps_backend/src/asteroids/asteroids.service.ts
+++ backend/space_apps_backend/src/asteroids/asteroids.service.ts
@@ -1,6 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { features } from 'node:process';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable()
@@ -31,7 +32,8 @@ export class AsteroidsService {
         this.mapsApiKey = this.configService.get<string>('MAPS_API_KEY')
     }
 
-    async getDiameterById(id: number) {
+    // TODO DARLE TAMBIEN LA MASA
+    getDiameterById(id: number) {
         if (this.asteroid_diameters.has(id)) {
           return this.asteroid_diameters.get(id)!;
         } else {
@@ -42,13 +44,35 @@ export class AsteroidsService {
         } 
     }
 
+    getAsteroidInfo(id: number) {
+        const diameter_kilometers = this.getDiameterById(id)
+        const diameter_centimeters = diameter_kilometers * 100_000;
+        const area = Math.pow((diameter_centimeters /2),2) * Math.PI * 4;
+        const masa = (area * 5) / 1_000;
+
+        return {
+            mass: masa / 50,
+            diameter: diameter_kilometers
+        }
+    }
+
+    
+    getExplosionRadius(velocity: number, mass: number) {
+        let E = 0.5 * mass * velocity * velocity;
+        console.log(`E: ${E}`)
+        let W = E / (4.18e6);
+        console.log(`W: ${W}`)
+        let R = Math.pow(W, 1/3) * 4.5;
+        return R;
+    }
+
     async getMapImage(latitude: number, longitude: number, diameter: number) {
-        const url = `${this.maps_api}?center=${latitude},${longitude}&scale=2&zoom=15&size=1920x1080&format=png&maptype=satellite&key=${this.mapsApiKey}`
+        const zoom = this.getZoom(diameter);
+        const url = `${this.maps_api}?center=${latitude},${longitude}&scale=2&zoom=${zoom}&size=1920x1080&format=png&maptype=satellite&key=${this.mapsApiKey}`
 
         try {
             const response = await firstValueFrom(
-            this.httpService.get(url, { responseType: 'arraybuffer' })
-            );
+            this.httpService.get(url, { responseType: 'arraybuffer' }));
 
             return Buffer.from(response.data, 'binary');
         }
@@ -57,5 +81,43 @@ export class AsteroidsService {
         }
     }
 
+    getZoom(diameter: number) {
+        if (diameter < 125) return 19;
+        else if (diameter < 250) return 18;
+        else if (diameter < 500) return 17;
+        else if (diameter < 1000) return 16;
+        else if (diameter < 2000) return 15;
+        else if (diameter < 4000) return 14;
+        else if (diameter < 8000) return 13;
+        else if (diameter < 16000) return 12;
+        else if (diameter < 32000) return 11;
+        else if (diameter < 32000) return 10;
+        else if (diameter < 64000) return 9;
+        else if (diameter < 128000) return 8;
+        else if (diameter < 256000) return 7;
+        else if (diameter < 512000) return 6;
+        else if (diameter < 1024000) return 5;
+        else return 3;
+
+    }
+
+    async getZoneInfo(longitud: number, latitud: number, radio_metros: number) {
+        const url = `https://api.geoapify.com/v2/places?categories=populated_place&filter=circle:${longitud},${latitud},${radio_metros}&bias=proximity:${longitud},${latitud}&limit=20&apiKey=f96d276a61904cf08414922723b78977`;
+        try {
+            const response = await firstValueFrom(this.httpService.get(url));
+            let population_count = 0;
+            response.data.features.forEach(feature => {
+                let population = feature?.properties?.datasource?.raw?.population;
+                if (population) {
+                    population_count += population;
+                }
+            });
+
+            return population_count;
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
 
 }
